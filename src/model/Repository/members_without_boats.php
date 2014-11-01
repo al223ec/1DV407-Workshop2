dc5fec8c06eaf6fb5c061e0775f90497e505399f
afrxx09 <afrxx09@student.lnu.se>
2014-11-01 11:31:14 +0100

    Bug show members without boats

--- src/model/Repository/members_without_boats.php
+++ src/model/Repository/members_without_boats.php
@@ -5,20 +5,22 @@ class MembersWithoutBoats extends \core\Repository implements IMemberFilterRepos
 
 	public function getFilterdMembers(){
 		$sql = "
-			SELECT member.*
+			SELECT member.*, boat.id as boat_id
 			FROM member
-			RIGHT JOIN boat
-			ON member.id != boat.member_id
+			LEFT JOIN boat
+			ON member.id = boat.member_id
 			GROUP BY member.id
 		";  
 		$ret = array(); 
 
 		if($response = $this->query($sql)){
 			foreach ($response as $memberdbo) {
-				$member = new \model\Member($memberdbo['id']); 
-				$member->setName($memberdbo['name']); 
-				$member->setSsn($memberdbo['ssn']); 
-				$ret[] = $member; 
+				if($memberdbo['boat_id'] == null){
+					$member = new \model\Member($memberdbo['id']); 
+					$member->setName($memberdbo['name']); 
+					$member->setSsn($memberdbo['ssn']); 
+					$ret[] = $member; 
+				}
 			}
 		} 
 		return $ret; 
